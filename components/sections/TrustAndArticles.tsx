f548b1c971e6d55b23eb811afac4f9e9b14509cf
Webeside Technology <webesidetechnologyindia@gmail.com>
2026-01-26 19:41:50 +0530

    update

--- components/sections/TrustAndArticles.tsx
+++ components/sections/TrustAndArticles.tsx
@@ -10,7 +10,7 @@ const TrustAndArticles = () => {
   const articles = [
     { title: 'Off-the-shelf AI platform or Custom AI Agent solution?', img: 'https://img.freepik.com/free-photo/robot-working-as-cashier-instead-humans_23-2150911987.jpg' },
     { title: 'Choosing the right LLM model for the job', img: 'https://img.freepik.com/free-photo/medium-shot-smiley-colleagues-work_23-2149622840.jpg' },
-    { title: 'What is Retrieval-Augmented Generation (RAG) for LLMs', img: 'https://vstorm.co/app/uploads/2024/04/Vstorm-operating-5-1.jpg' },
+    { title: 'What is Retrieval-Augmented Generation (RAG) for LLMs', img: 'https://img.freepik.com/free-photo/young-guy-aged-man-with-vr-glasses-sitting-settee_23-2148030187.jpg' },
     { title: 'CLC: Leader in LLMs solutions recognized by Deloitte...', img: 'https://img.freepik.com/free-photo/career-progression-promotion-achievement-success-concept_53876-120915.jpg' },
   ];
 
